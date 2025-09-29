0c9b872c236f136784d793a16d93b6c210a1b6bd
yhz994710 <2739904664@qq.com>
2025-09-29 09:20:55 +0000

    [PT2E] Add support for int8 conv+relu and linear+relu.

--- torchao/quantization/pt2e/reference_representation_rewrite.py
+++ torchao/quantization/pt2e/reference_representation_rewrite.py
@@ -93,33 +93,92 @@ def _reference_quantized_linear(
     x_i8 = torch.ops.aten.clamp(x_i8, x_quant_min, x_quant_max)
     weight_i8 = torch.ops.aten.clamp(weight_i8, weight_quant_min, weight_quant_max)
 
-    x_i16 = x_i8.to(torch.int16)
-    weight_i16 = weight_i8.to(torch.int16)
     # always set bias to None so that the same representation can work for the case
     # no matter if bias_scale == x_scale * weight_scale or not
-    acc_i32 = out_dtype(
-        torch.ops.aten.linear.default,
-        torch.int32,
-        x_i16 - x_zero_point,
-        weight_i16 - weight_zero_point,
-        None,
-    )
+    acc_i32 = torch.ops.aten.linear.default(x_i8 - x_zero_point, weight_i8 - weight_zero_point, None).to(torch.int32)
+    # acc_i32 = torch.ops.aten.linear.default((x_i8 - x_zero_point).to(torch.int32), (weight_i8 - weight_zero_point).to(torch.int32), None).to(torch.int32)
     # TODO: change to mul.Scalar
     # Note: we are quantizing bias with these scales without signal from user, but it might be OK
     bias_scale = x_scale * weight_scale
-    bias_i32 = out_dtype(torch.ops.aten.div.Tensor, torch.int32, bias_fp32, bias_scale)
+    bias_i32 = torch.ops.aten.div.Tensor(bias_fp32, bias_scale).to(torch.int32)
     acc_i32 = acc_i32 + bias_i32
     # TODO: change to mul.Scalar when we make x_scale/weight_scale etc. Scalar values
-    acc_i32 = (
-        out_dtype(
-            torch.ops.aten.mul.Tensor,
-            torch.int32,
-            acc_i32,
-            x_scale * weight_scale / out_scale,
-        )
-        + out_zero_point
-    )
+    acc_i32 = (torch.ops.aten.mul.Tensor(acc_i32.to(torch.float32), x_scale * weight_scale / out_scale)).to(torch.int32) + out_zero_point
     out_i8 = torch.ops.aten.clamp(acc_i32, out_quant_min, out_quant_max).to(torch.int8)
+    return out_i8
+
+
+def _qdq_quantized_linear_relu(
+    x_i8,
+    x_scale,
+    x_zero_point,
+    x_quant_min,
+    x_quant_max,
+    weight_i8,
+    weight_scale,
+    weight_zero_point,
+    weight_quant_min,
+    weight_quant_max,
+    bias_fp32,
+    out_scale,
+    out_zero_point,
+    out_quant_min,
+    out_quant_max,
+):
+    x_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
+        x_i8, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8
+    )
+    weight_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
+        weight_i8,
+        weight_scale,
+        weight_zero_point,
+        weight_quant_min,
+        weight_quant_max,
+        torch.int8,
+    )
+    out_fp32 = torch.ops.aten.linear.default(x_fp32, weight_fp32, bias_fp32)
+    out_fp32 = torch.ops.aten.relu_(out_fp32)
+    out_i8 = torch.ops.quantized_decomposed.quantize_per_tensor(
+        out_fp32, out_scale, out_zero_point, out_quant_min, out_quant_max, torch.int8
+    )
+    return out_i8
+
+
+def _reference_quantized_linear_relu(
+    x_i8,
+    x_scale,
+    x_zero_point,
+    x_quant_min,
+    x_quant_max,
+    weight_i8,
+    weight_scale,
+    weight_zero_point,
+    weight_quant_min,
+    weight_quant_max,
+    bias_fp32,
+    out_scale,
+    out_zero_point,
+    out_quant_min,
+    out_quant_max,
+):
+    # without using quant_min/max in clamp, the traced graph will not have quant_mi/max args.
+    # This results in failure to match the pattern.
+    # Therefore, we call a torch.ops.aten.clamp here
+    x_i8 = torch.ops.aten.clamp(x_i8, x_quant_min, x_quant_max)
+    weight_i8 = torch.ops.aten.clamp(weight_i8, weight_quant_min, weight_quant_max)
+
+    # always set bias to None so that the same representation can work for the case
+    # no matter if bias_scale == x_scale * weight_scale or not
+    acc_i32 = torch.ops.aten.linear.default(x_i8 - x_zero_point, weight_i8 - weight_zero_point, None).to(torch.int32)
+    # acc_i32 = torch.ops.aten.linear.default((x_i8 - x_zero_point).to(torch.int32), (weight_i8 - weight_zero_point).to(torch.int32), None).to(torch.int32)
+    # TODO: change to mul.Scalar
+    # Note: we are quantizing bias with these scales without signal from user, but it might be OK
+    bias_scale = x_scale * weight_scale
+    bias_i32 = torch.ops.aten.div.Tensor(bias_fp32, bias_scale).to(torch.int32)
+    acc_i32 = acc_i32 + bias_i32
+    # TODO: change to mul.Scalar when we make x_scale/weight_scale etc. Scalar values
+    acc_i32 = (torch.ops.aten.mul.Tensor(acc_i32.to(torch.float32), x_scale * weight_scale / out_scale)).to(torch.int32) + out_zero_point
+    out_i8 = torch.ops.aten.clamp(acc_i32, out_zero_point, out_quant_max).to(torch.int8)
     return out_i8
 
 
@@ -221,10 +280,10 @@ def _qdq_quantized_conv2d(
     out_quant_max,
 ):
     stride = [1, 1]
-    padding = [0, 0]
+    padding = [1, 1]
     dilation = [1, 1]
-    transposed = False
-    output_padding = [0, 0]
+    # transposed = False
+    # output_padding = [0, 0]
     groups = 1
     x_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
         x_i8, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8
@@ -237,15 +296,15 @@ def _qdq_quantized_conv2d(
         weight_quant_max,
         torch.int8,
     )
-    out_fp32 = torch.ops.aten.convolution.default(
+    out_fp32 = torch.ops.aten.conv2d.default(
         x_fp32,
         weight_fp32,
         bias_fp32,
         stride,
         padding,
         dilation,
-        transposed,
-        output_padding,
+        # transposed,
+        # output_padding,
         groups,
     )
     out_i8 = torch.ops.quantized_decomposed.quantize_per_tensor(
@@ -272,10 +331,10 @@ def _reference_quantized_conv2d(
     out_quant_max,
 ):
     stride = [1, 1]
-    padding = [0, 0]
+    padding = [1, 1]
     dilation = [1, 1]
-    transposed = False
-    output_padding = [0, 0]
+    # transposed = False
+    # output_padding = [0, 0]
     groups = 1
     # without using quant_min/max in clamp, the traced graph will not have quant_mi/max args.
     # This results in failure to match the pattern.
@@ -287,19 +346,42 @@ def _reference_quantized_conv2d(
     weight_i16 = weight_i8.to(torch.int16)
     # always set bias to None so that the same representation can work for the case
     # no matter if bias_scale == x_scale * weight_scale or not
-    acc_i32 = out_dtype(
-        torch.ops.aten.convolution.default,
-        torch.int32,
-        x_i16 - x_zero_point,
-        weight_i16 - weight_zero_point,
+    acc_i32 = torch.ops.aten.conv2d.default(
+        x_i8 - x_zero_point,
+        weight_i8 - weight_zero_point,
         None,
         stride,
         padding,
         dilation,
-        transposed,
-        output_padding,
+        # transposed,
+        # output_padding,
         groups,
     )
+    # acc_i32 = torch.ops.aten.conv2d.default(
+    #     (x_i8 - x_zero_point).to(torch.int32),
+    #     (weight_i8 - weight_zero_point).to(torch.int32),
+    #     None,
+    #     stride,
+    #     padding,
+    #     dilation,
+    #     # transposed,
+    #     # output_padding,
+    #     groups,
+    # )
+    acc_i32 = acc_i32.to(torch.int32)
+    # acc_i32 = out_dtype(
+    #     torch.ops.aten.conv2d.default,
+    #     torch.int32,
+    #     x_i16 - x_zero_point,
+    #     weight_i16 - weight_zero_point,
+    #     None,
+    #     stride,
+    #     padding,
+    #     dilation,
+    #     # transposed,
+    #     # output_padding,
+    #     groups,
+    # )
     # Note: we are quantizing bias with these scales without signal from user, but it might be OK
     bias_scale = x_scale * weight_scale
     # bias quantization to int32 uses bias_scale = x_scale * weight_scale due to:
@@ -315,7 +397,8 @@ def _reference_quantized_conv2d(
     # Note we had to multiply bias_fp32 qith X_scale * W_scale = bias_scale
     # Thus bias quantization to int32 must be with X_scale * W_scale
 
-    bias_i32 = out_dtype(torch.ops.aten.div.Tensor, torch.int32, bias_fp32, bias_scale)
+    # bias_i32 = out_dtype(torch.ops.aten.div.Tensor, torch.int32, bias_fp32, bias_scale)
+    bias_i32 = torch.ops.aten.div.Tensor(bias_fp32, bias_scale).to(torch.int32)
     # Unsqueeze to match broadcast dims
     # Unfortnuately I cannot do bias_i32.unsqueeze(0) due to literal matching nightmare
     # in graph pattern replacement
@@ -323,16 +406,146 @@ def _reference_quantized_conv2d(
     bias_i32 = bias_i32.unsqueeze(-1)
     acc_i32 = acc_i32 + bias_i32
     # TODO: change to mul.Scalar when we make x_scale/weight_scale etc. Scalar values
-    acc_i32 = (
-        out_dtype(
-            torch.ops.aten.mul.Tensor,
-            torch.int32,
-            acc_i32,
-            x_scale * weight_scale / out_scale,
-        )
-        + out_zero_point
-    )
+    # acc_i32 = (
+    #     out_dtype(
+    #         torch.ops.aten.mul.Tensor,
+    #         torch.int32,
+    #         acc_i32,
+    #         x_scale * weight_scale / out_scale,
+    #     )
+    #     + out_zero_point
+    # )
+    acc_i32 = torch.ops.aten.mul.Tensor(acc_i32, x_scale * weight_scale / out_scale) + out_zero_point
+    acc_i32 = acc_i32.to(torch.int32)
     out_i8 = torch.ops.aten.clamp(acc_i32, out_quant_min, out_quant_max).to(torch.int8)
+    return out_i8
+
+
+def _qdq_quantized_conv2d_relu(
+    x_i8,
+    x_scale,
+    x_zero_point,
+    x_quant_min,
+    x_quant_max,
+    weight_i8,
+    weight_scale,
+    weight_zero_point,
+    weight_quant_min,
+    weight_quant_max,
+    bias_fp32,
+    out_scale,
+    out_zero_point,
+    out_quant_min,
+    out_quant_max,
+):
+    stride = [1, 1]
+    padding = [1, 1]
+    dilation = [1, 1]
+    transposed = False
+    output_padding = [0, 0]
+    groups = 1
+    x_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
+        x_i8, x_scale, x_zero_point, x_quant_min, x_quant_max, torch.int8
+    )
+    weight_fp32 = torch.ops.quantized_decomposed.dequantize_per_tensor(
+        weight_i8,
+        weight_scale,
+        weight_zero_point,
+        weight_quant_min,
+        weight_quant_max,
+        torch.int8,
+    )
+    out_fp32 = torch.ops.aten.conv2d.default(
+        x_fp32,
+        weight_fp32,
+        bias_fp32,
+        stride,
+        padding,
+        dilation,
+        # transposed,
+        # output_padding,
+        groups,
+    )
+    out_fp32 = torch.ops.aten.relu_(out_fp32)
+    out_i8 = torch.ops.quantized_decomposed.quantize_per_tensor(
+        out_fp32, out_scale, out_zero_point, out_quant_min, out_quant_max, torch.int8
+    )
+    return out_i8
+
+
+def _reference_quantized_conv2d_relu(
+    x_i8,
+    x_scale,
+    x_zero_point,
+    x_quant_min,
+    x_quant_max,
+    weight_i8,
+    weight_scale,
+    weight_zero_point,
+    weight_quant_min,
+    weight_quant_max,
+    bias_fp32,
+    out_scale,
+    out_zero_point,
+    out_quant_min,
+    out_quant_max,
+):
+    stride = [1, 1]
+    padding = [1, 1]
+    dilation = [1, 1]
+    groups = 1
+    # without using quant_min/max in clamp, the traced graph will not have quant_mi/max args.
+    # This results in failure to match the pattern.
+    # Therefore, we call a torch.ops.aten.clamp here
+    x_i8 = torch.ops.aten.clamp(x_i8, x_quant_min, x_quant_max)
+    weight_i8 = torch.ops.aten.clamp(weight_i8, weight_quant_min, weight_quant_max)
+
+    # always set bias to None so that the same representation can work for the case
+    # no matter if bias_scale == x_scale * weight_scale or not
+    acc_i32 = torch.ops.aten.conv2d.default(
+        x_i8 - x_zero_point,
+        weight_i8 - weight_zero_point,
+        None,
+        stride,
+        padding,
+        dilation,
+        groups,
+    )
+    # acc_i32 = torch.ops.aten.conv2d.default(
+    #     (x_i8 - x_zero_point).to(torch.int32),
+    #     (weight_i8 - weight_zero_point).to(torch.int32),
+    #     None,
+    #     stride,
+    #     padding,
+    #     dilation,
+    #     groups,
+    # )
+    acc_i32 = acc_i32.to(torch.int32)
+    # Note: we are quantizing bias with these scales without signal from user, but it might be OK
+    bias_scale = x_scale * weight_scale
+    # bias quantization to int32 uses bias_scale = x_scale * weight_scale due to:
+    # Take linear calculation for example
+    # Out_(i, j)_fp32 = Sum_(over k)[X_(i, k)_fp32 * W_(i, k)_fp32] + bias_(i)_fp32
+    # Represent X, W fp32 as their dequant transforms
+    # A_fp32 = (A_q - A_zero_point)/A_scale
+    # Out_(i, j)_fp32 = Sum_(over k)[(X_(i, k)_fp32 - X_zp) * X_scale * (W_(i, k)_fp32 - W_zp) * W_scale] + bias_(i)_fp32
+    # Factor out X_scale and W_scale
+    # Out_(i, j)_fp32 = ((X_scale * W_scale) * Sum_(over k)[(X_(i, k)_fp32 - X_zp) * (W_(i, k)_fp32 - W_zp)]) + bias_(i)_fp32
+    # In order to addition of bias_(i)_fp32 inside, we must do
+    # Out_(i, j)_fp32 = (X_scale * W_scale) * (Sum_(over k)[(X_(i, k)_fp32 - X_zp) * (W_(i, k)_fp32 - W_zp)] + (1 / (X_scale * W_scale)) * bias_(i)_fp32)W_scale  # noqa: B950
+    # Note we had to multiply bias_fp32 qith X_scale * W_scale = bias_scale
+    # Thus bias quantization to int32 must be with X_scale * W_scale
+
+    bias_i32 = torch.ops.aten.div.Tensor(bias_fp32, bias_scale).to(torch.int32)
+    # Unsqueeze to match broadcast dims
+    # Unfortnuately I cannot do bias_i32.unsqueeze(0) due to literal matching nightmare
+    # in graph pattern replacement
+    bias_i32 = bias_i32.unsqueeze(-1)
+    bias_i32 = bias_i32.unsqueeze(-1)
+    acc_i32 = acc_i32 + bias_i32
+    # TODO: change to mul.Scalar when we make x_scale/weight_scale etc. Scalar values
+    acc_i32 = (torch.ops.aten.mul.Tensor(acc_i32.to(torch.float32), x_scale * weight_scale / out_scale)).to(torch.int32) + out_zero_point
+    out_i8 = torch.ops.aten.clamp(acc_i32, out_zero_point, out_quant_max).to(torch.int8)
     return out_i8
 
 
@@ -760,9 +973,23 @@ def reference_representation_rewrite(model: GraphModule) -> GraphModule:
             _replace_literals_with_new_placeholders,
         ),
         _RewriteInfo(
+            _QUANTIZED_LINEAR_EXAMPLE_INPUTS,
+            WrapperModule(_qdq_quantized_linear_relu),
+            WrapperModule(_reference_quantized_linear_relu),
+            _replace_literals_with_new_placeholders,
+            _replace_literals_with_new_placeholders,
+        ),
+        _RewriteInfo(
             _QUANTIZED_CONV2d_EXAMPLE_INPUTS,
             WrapperModule(_qdq_quantized_conv2d),
             WrapperModule(_reference_quantized_conv2d),
+            partial(_replace_literals_with_new_placeholders, exclude_literals=[-1]),
+            partial(_replace_literals_with_new_placeholders, exclude_literals=[-1]),
+        ),
+        _RewriteInfo(
+            _QUANTIZED_CONV2d_EXAMPLE_INPUTS,
+            WrapperModule(_qdq_quantized_conv2d_relu),
+            WrapperModule(_reference_quantized_conv2d_relu),
             partial(_replace_literals_with_new_placeholders, exclude_literals=[-1]),
             partial(_replace_literals_with_new_placeholders, exclude_literals=[-1]),
         ),
